5bdb12a5fe5793bb8b3b5409a7f961a32caaa65d
Sam Falcon <samfalc@gmail.com>
2022-02-13 23:56:06 -0500

    standup agm and set map location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bill-frontend-app';
+export class AppComponent implements OnInit {
+  latitude: number;
+  longitude: number;
+  title = 'Bill.com Frontend App';
+  zoom: number;
+
+  ngOnInit(): void {
+    this.setLocation();
+  }
+
+  handleSearch(evt: Event): void {
+    // TODO: handle search
+  }
+
+  /*
+   * Set map location to Bill.com Headquarters
+   */
+  private setLocation(): void {
+    this.latitude = 37.41883;
+    this.longitude = -121.97839;
+    this.zoom = 16;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AgmCoreModule } from '@agm/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      // TODO: move this to SSM Param
+      apiKey: 'AIzaSyBAS8w7gAcvHwHRUHKmRrqQG7Z8FiJoyM4',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
